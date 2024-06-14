140ff767b86d963524161aa5dbb2a8f7f3fe92e0
Evan Lim <evan.lim@berkeley.edu>
2024-06-13 23:05:27 -0700

    tweaked console messages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,14 +8,14 @@ const DB_ID = 'ec4117c0-0af7-4edd-9eb4-99fa46c4590c';
 const ORG_ID = "d8be229a-5116-4b28-ade5-da13199cd3fb";
 
 
-function SearchBar({ onSearch }) {
+function SearchBar({ searchType, onSearch }) {
   const inputRef = useRef(null);
 
   const handleKeyUp = (event) => {
     if (event.key === 'Enter') {
       inputRef.current.blur(); 
     }
-    onSearch();
+    onSearch(searchType);
   };
 
   return (
@@ -28,7 +28,7 @@ function SearchBar({ onSearch }) {
         onKeyUp={handleKeyUp}
         autoFocus
       />
-      <img className="search-button" src={ require('./search.png')} onClick={onSearch}/>
+      <img className="search-button" src={ require('./search.png')} onClick={() => onSearch(searchType)}/>
     </div>
   );
 }
@@ -47,63 +47,64 @@ function ModeMenu({ onModeClick }) {
 }
 
 export default function App() {
-  const [mode, setMode] = useState("Semantic");
+  const [mode, setMode] = useState("semantic");
   const [searchResults, setSearchResults] = useState([]);
 
-  function handleMode(mode) {
-    setMode(mode);
+  function handleMode(searchType) {
+    setMode(searchType);
     let modeOptions = Array.from(document.getElementsByClassName("mode-option"));
     modeOptions.forEach((option) => {
       option.classList.remove("selected");
     });
-    document.querySelector("." + mode).classList.add("selected");
+    document.querySelector("." + searchType).classList.add("selected");
+    handleSearch(searchType);
   }
 
   async function fetchSearchResults(query, searchType) {
-    console.log(`searching for: ${query} with search type: ${searchType}`)
-    // const options = {
-    //   method: 'POST',
-    //   headers: {
-    //     Authorization: API_KEY,
-    //     'TR-Dataset': DB_ID,
-    //     "TR-Organization": ORG_ID,
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     date_bias: true,
-    //     page: 1,
-    //     page_size: 10,
-    //     query: query,
-    //     score_threshold: 0.5,
-    //     search_type: searchType,
-    //     use_weights: true
-    //   }),
-    // };
-    
-    // try {
-    //   const response = await fetch('https://api.trieve.ai/api/chunk/search', options);
-    //   if (!response.ok) {
-    //     throw new Error(`HTTP error! Status: ${response.status}`);
-    //   }
-    //   console.log(data);
-    //   const data = await response.json();
-    //   setSearchResults(data.results);
-    // } catch (error) {
-    //   console.error('Error fetching search results:', error);
-    //   setSearchResults([]);
-    // }
+    console.log(`searching for: "${query}" with search type: "${searchType}"`)
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: API_KEY,
+        'TR-Dataset': DB_ID,
+        "TR-Organization": ORG_ID,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        date_bias: true,
+        page: 1,
+        page_size: 10,
+        query: query,
+        score_threshold: 0.5,
+        search_type: searchType,
+        use_weights: true
+      }),
+    };
+    console.log(options);
+    try {
+      const response = await fetch('https://api.trieve.ai/api/chunk/search', options);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      console.log(data);
+      const data = await response.json();
+      setSearchResults(data.results);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+      setSearchResults([]);
+    }
   }
 
-  async function handleSearch() {
+  async function handleSearch(searchType) {
     const searchText = document.querySelector(".search-input").value;
-    await fetchSearchResults(searchText, mode);
+    await fetchSearchResults(searchText, searchType);
   }
 
   return (
     <div className="content">
       <div className="title">thrift<b>books</b> trieve search</div>
       <div className="description">Read more. Search less.</div>
-      <SearchBar onSearch={handleSearch}/>
+      <SearchBar searchType={mode} onSearch={handleSearch}/>
       <div className="mode-menu unselectable">
         <ModeMenu onModeClick={ handleMode } />
       </div>
